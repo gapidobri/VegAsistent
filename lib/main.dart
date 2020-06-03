86c78d1ad45e0c8ca06b6f0528f6790a72fcc5bf
gapidobri <gapidobri@users.noreply.github.com>
2020-06-04 00:34:24 +0200

    Started on calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'timetable/timetable.dart';
+import 'package:flutter/services.dart';
+import 'package:time_machine/time_machine.dart';
+import 'package:vegasistent/timetable/calendar.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await TimeMachine.initialize({'rootBundle': rootBundle});
+
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Timetable(),
+      home: Calendar(),
     );
   }
 }
